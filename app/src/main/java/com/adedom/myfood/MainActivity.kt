8fed9dcd433df338cd18b1fce96db9425bc4a06d
pathiphon <pathiphon.ja@chococrm.com>
2022-02-20 20:24:52 +0700

    Implement kodein.

--- app/src/main/java/com/adedom/myfood/MainActivity.kt
+++ app/src/main/java/com/adedom/myfood/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.adedom.myfood
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,57 +14,48 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.adedom.core.presentation.main.flow.MainFlow
-import com.adedom.myfood.base.BaseActivity
 import com.adedom.myfood.ui.theme.MyfoodTheme
-import org.kodein.di.instance
+import org.kodein.di.compose.rememberInstance
 
-class MainActivity : BaseActivity() {
-
-    private val flow: MainFlow by instance()
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyfoodTheme {
-                val viewState = flow.viewState.collectAsState()
-
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Column {
-                        Greeting("Android")
-
-                        Text(text = "MainActivity : ${viewState.value.text}")
-
-                        Text(text = "Current time : ${viewState.value.currentTime}")
-
-                        TextField(
-                            value = viewState.value.text,
-                            onValueChange = flow::setTextChangeAction
-                        )
-                    }
-                }
+                App()
             }
         }
-    }
-
-    override fun onDestroy() {
-        flow.onCleared()
-        super.onDestroy()
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun App() {
+    val flow: MainFlow by rememberInstance()
+
+    val viewState = flow.viewState.collectAsState()
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column {
+            Text(text = "MainActivity : ${viewState.value.text}")
+
+            Text(text = "Current time : ${viewState.value.currentTime}")
+
+            TextField(
+                value = viewState.value.text,
+                onValueChange = flow::setTextChangeAction
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyfoodTheme {
-        Greeting("Android")
+        App()
     }
 }
--- app/src/main/java/com/adedom/myfood/MainApplication.kt
+++ app/src/main/java/com/adedom/myfood/MainApplication.kt
@@ -6,10 +6,13 @@ import com.adedom.core.di.presentationModule
 import com.adedom.core.di.repositoryModule
 import org.kodein.di.DI
 import org.kodein.di.DIAware
+import org.kodein.di.android.x.androidXModule
 
 class MainApplication : Application(), DIAware {
 
     override val di by DI.lazy {
+        import(androidXModule(this@MainApplication))
+
         importAll(
             repositoryModule,
             domainModule,
